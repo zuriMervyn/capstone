b972c8e2eb2ec54482e94af082467277ecfca83f
Zuri Mervyn <zuriank@gmail.com>
2023-05-16 16:03:45 -0700

    setting up html document

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header';
+import Nav from './Nav';
+import Main from './Main';
+import Footer from './Footer';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      homepage
+    <div>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
