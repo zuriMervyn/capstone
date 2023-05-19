8019e240410abc3d12b7f0bf1ca53df772fbb90a
Zuri Mervyn <zuriank@gmail.com>
2023-05-19 10:55:01 -0700

    finished ui

--- my-app/src/BookingForm.js
+++ my-app/src/BookingForm.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { fetchAPI } from './api';
 
 function BookingForm({ submitForm }) {
   const [date, setDate] = useState("");
@@ -6,18 +7,6 @@ function BookingForm({ submitForm }) {
   const [guests, setGuests] = useState("");
   const [occasion, setOccasion] = useState("");
   const [availableTimes, setAvailableTimes] = useState([]);
-
-  function fetchAPI(selectedDate) {
-    const endpoint = `https://raw.githubusercontent.com/Meta-Front-End-Developer-PC/capstone/master/api.js`;
-
-    return new Promise((resolve, reject) => {
-      // Simulating API call with setTimeout
-      setTimeout(() => {
-        const availableTimes = ["12:00 PM", "1:00 PM", "7:00 PM"]; // Example available times
-        resolve(availableTimes);
-      }, 1000);
-    });
-  }
 
   function initializeTimes() {
     const today = new Date();
@@ -53,25 +42,34 @@ function BookingForm({ submitForm }) {
     submitForm(e, formData);
   };
 
+  const isFormValid = date && time && guests && occasion;
+
   return (
     <form style={{ display: "grid", maxWidth: "200px", gap: "20px" }} onSubmit={handleSubmit}>
       <label htmlFor="res-date">Choose date</label>
-      <input type="date" id="res-date" value={date} onChange={(e) => setDate(e.target.value)} />
+      <input type="date" id="res-date" value={date} onChange={(e) => setDate(e.target.value)} required aria-label="Choose date" />
+
       <label htmlFor="res-time">Choose time</label>
-      <select id="res-time" value={time} onChange={(e) => setTime(e.target.value)}>
+      <select id="res-time" value={time} onChange={(e) => setTime(e.target.value)} required aria-label="Choose time">
+        <option value="">Select</option>
         {availableTimes.map((timeOption) => (
           <option key={timeOption}>{timeOption}</option>
         ))}
       </select>
+
       <label htmlFor="res-guests">Number of guests</label>
-      <input type="number" id="res-guests" value={guests} onChange={(e) => setGuests(e.target.value)} />
+      <input type="number" id="res-guests" value={guests} onChange={(e) => setGuests(e.target.value)} min={1} required aria-label="Number of guests" />
+
       <label htmlFor="res-occasion">Occasion</label>
-      <select id="res-occasion" value={occasion} onChange={(e) => setOccasion(e.target.value)}>
+      <select id="res-occasion" value={occasion} onChange={(e) => setOccasion(e.target.value)} required aria-label="Occasion">
         <option value="">Select</option>
         <option value="Birthday">Birthday</option>
         <option value="Anniversary">Anniversary</option>
       </select>
-      <button type="submit">Submit Reservation</button>
+
+      <button type="submit" disabled={!isFormValid} aria-label="Submit Reservation">
+        Submit Reservation
+      </button>
     </form>
   );
 }
